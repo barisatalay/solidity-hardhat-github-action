fd2923aef8abce6c8543a97a9effb2eb13295e11
Barış ATALAY <b.atalay007@gmail.com>
2024-04-26 15:38:40 +0300

    Add hardhat-gas-reporter plugin to project dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  gasReporter: {
+    enabled: (process.env.REPORT_GAS) ? true : true,
+    currency: process.env.REPORT_CURRENCY,
+    coinmarketcap: process.env.COINGECKO_API_KEY,
+    token: process.env.REPORT_MAIN_TOKEN,
+    showTimeSpent: true,   
+  }
 };
 
 export default config;
